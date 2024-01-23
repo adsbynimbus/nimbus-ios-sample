28c6b61a6240a79ecf2fa64e096690b34c403b7e
Standa Musil <standa@nimbus.co>
2024-01-23 12:42:17 -0800

    import foundation for tests to pass

--- Application/Sources/Mediation/GoogleAdManager/GAMConstants.swift
+++ Application/Sources/Mediation/GoogleAdManager/GAMConstants.swift
@@ -5,5 +5,7 @@
 //  Copyright © 2024 Nimbus Advertising Solutions Inc. All rights reserved.
 //
 
+import Foundation
+
 let googleDynamicPricePlacementId = Bundle.main.infoDictionary?["Google Nimbus Rendering Placement ID"] as? String ?? ""
 let googleDynamicPriceRewardedPlacementId = Bundle.main.infoDictionary?["Google Nimbus Rendering Rewarded Placement ID"] as? String ?? ""
