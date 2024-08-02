d1d2d319c68901810c2aeeb393698b0121518a7a
Standa Musil <standa@nimbus.co>
2024-08-02 11:10:26 -0700

    move NimbusAd creation to TestRenderViewController for easier manipulation. Allow to send use_new_renderer field (#113)

--- Application/Sources/Test/TestRenderAdViewController.swift
+++ Application/Sources/Test/TestRenderAdViewController.swift
@@ -9,17 +9,14 @@ import UIKit
 import NimbusKit
 
 final class TestRenderAdViewController: UIViewController {
-    private let adMarkup: String
-    private let iTunesAppId: String?
+    private let ad: NimbusAd
     
     private lazy var adContainerView: CustomAdContainerView = {
-        return CustomAdContainerView(ad: Self.getAdFromMarkup(adMarkup: adMarkup), viewController: self)
+        return CustomAdContainerView(ad: ad, viewController: self)
     }()
     
-    init(adMarkup: String, iTunesAppId: String?) {
-        self.adMarkup = adMarkup
-        self.iTunesAppId = iTunesAppId
-        
+    init(ad: NimbusAd) {
+        self.ad = ad
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -27,10 +24,10 @@ final class TestRenderAdViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
-    static func showBlocking(from: UIViewController, adMarkup: String, iTunesAppId: String? = nil) {
+    static func showBlocking(from: UIViewController, ad: NimbusAd) {
         let adView = NimbusAdView(adPresentingViewController: from)
-        adView.showsSKOverlay = iTunesAppId != nil
-        let controller = NimbusAdViewController(adView: adView, ad: getAdFromMarkup(adMarkup: adMarkup, iTunesAppId: iTunesAppId), companionAd: nil)
+        adView.showsSKOverlay = ad.extensions?.skAdNetwork != nil
+        let controller = NimbusAdViewController(adView: adView, ad: ad, companionAd: nil)
         controller.modalPresentationStyle = .fullScreen
         
         adView.adPresentingViewController = controller
@@ -61,48 +58,5 @@ final class TestRenderAdViewController: UIViewController {
             child.alignTop()
             child.fill()
         }
-    }
-    
-    static private func getAdFromMarkup(adMarkup: String, iTunesAppId: String? = nil) -> NimbusAd {
-        let type: NimbusAuctionType = isVideoMarkup(adMarkup: adMarkup) ? .video : .static
-        return createNimbusAd(auctionType: type, markup: adMarkup, iTunesAppId: iTunesAppId)
-    }
-    
-    static private func isVideoMarkup(adMarkup: String) -> Bool {
-        let prefix = adMarkup.prefix(5).lowercased()
-        return prefix == "<vast" || prefix == "<?xml"
-    }
-    
-    static private func createNimbusAd(
-        placementId: String? = nil,
-        auctionType: NimbusAuctionType,
-        markup: String,
-        isMraid: Bool = true,
-        isInterstitial: Bool = true,
-        iTunesAppId: String? = nil
-    ) -> NimbusAd {
-        let adDimensions = isInterstitial ?
-        NimbusAdDimensions(width: 320, height: 480) :
-        NimbusAdDimensions(width: 300, height: 50)
-        
-        let ext = NimbusAdExtensions(skAdNetwork: NimbusAdSkAdNetwork(advertisedAppStoreItemID: iTunesAppId))
-        
-        return NimbusAd(
-            position: "",
-            auctionType: auctionType,
-            bidRaw: 0,
-            bidInCents: 0,
-            contentType: "",
-            auctionId: "",
-            network: "test_render",
-            markup: markup,
-            isInterstitial: isInterstitial,
-            placementId: nil,
-            duration: nil,
-            adDimensions: adDimensions,
-            trackers: nil,
-            isMraid: isMraid,
-            extensions: iTunesAppId != nil ? ext : nil
-        )
     }
 }
