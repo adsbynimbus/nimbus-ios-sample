59ed94c6256e5730a6f3651174a93a76ea091dc4
Standa Musil <standa@nimbus.co>
2024-11-12 11:33:24 -0800

    Simplify inspectable web view (#119)
    * remove UI test helpers previously added for Maestro that native UI tests dont need
    
    * make webView inspectable in the sample app
    
    * use destination instead of -sdk
    
    * run cocoapods on Xcode 16

--- Application/Sources/UI/UITest+Extension.swift
+++ Application/Sources/UI/UITest+Extension.swift
@@ -20,30 +20,11 @@ extension NimbusAd {
 }
 
 extension UIView {
-    
-    func setUiTestIdentifiers(for nimbusAd: NimbusAd, refreshing: Bool = false) {
-        setUiTestIdentifiers(
-            for: nimbusAd.testIdentifier,
-            id: refreshing ? "nimbus_refreshing_controller" : "nimbus_ad_view"
-        )
-    }
-    
-    func setUiTestIdentifiers(for adString: String, id: String) {
-        accessibilityContainerType = .semanticGroup
-        accessibilityIdentifier = id
-        accessibilityLabel = adString
-        accessibilityValue = adString
+    func makeWebViewInspectable() {
         subviews.forEach {
             if #available(macOS 13.3, iOS 16.4, tvOS 16.4, *), let webView = $0 as? WKWebView {
                 webView.isInspectable = true
             }
         }
-    }
-    
-    func setCompanionAdUiTestIdentifiers(for nimbusAd: NimbusAd) {
-        setUiTestIdentifiers(
-            for: nimbusAd.testIdentifier,
-            id: "nimbus_ad_view_companion"
-        )
     }
 }
