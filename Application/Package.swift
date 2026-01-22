dc0f8830db8fb3c6eac2db7a52b88eaeec27b572
Standa Musil <standa@nimbus.co>
2026-01-22 09:35:11 -0800

    update SPM dependencies to the new structure

--- Application/Package.swift
+++ Application/Package.swift
@@ -11,23 +11,32 @@ let package = Package(
             targets: ["Application"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/adsbynimbus/nimbus-ios-sdk.git", from: "2.22.0"),
+        .package(url: "https://github.com/adsbynimbus/nimbus-ios-sdk", from: "3.0.0"),
+        .package(url: "https://github.com/adsbynimbus/nimbus-ios-admob", from: "3.0.0"),
+        .package(url: "https://github.com/adsbynimbus/nimbus-ios-aps", from: "3.0.0"),
+        .package(url: "https://github.com/adsbynimbus/nimbus-ios-inmobi", from: "3.0.0"),
+        .package(url: "https://github.com/adsbynimbus/nimbus-ios-liveramp", from: "3.0.0"),
+        .package(url: "https://github.com/adsbynimbus/nimbus-ios-meta", from: "3.0.0"),
+        .package(url: "https://github.com/adsbynimbus/nimbus-ios-mintegral", from: "3.0.0"),
+        .package(url: "https://github.com/adsbynimbus/nimbus-ios-mobilefuse", from: "3.0.0"),
+        .package(url: "https://github.com/adsbynimbus/nimbus-ios-moloco", from: "3.0.0"),
+        .package(url: "https://github.com/adsbynimbus/nimbus-ios-unity", from: "3.0.0"),
+        .package(url: "https://github.com/adsbynimbus/nimbus-ios-vungle", from: "3.0.0"),
     ],
     targets: [
         .target(
             name: "Application",
             dependencies: [
                 .product(name: "NimbusKit", package: "nimbus-ios-sdk"),
-                .product(name: "NimbusAPSKit", package: "nimbus-ios-sdk"),
-                .product(name: "NimbusGAMKit", package: "nimbus-ios-sdk"),
-                .product(name: "NimbusMetaKit", package: "nimbus-ios-sdk"),
-                .product(name: "NimbusVungleKit", package: "nimbus-ios-sdk"),
-                .product(name: "NimbusUnityKit", package: "nimbus-ios-sdk"),
-                .product(name: "NimbusMobileFuseKit", package: "nimbus-ios-sdk"),
-                .product(name: "NimbusAdMobKit", package: "nimbus-ios-sdk"),
-                .product(name: "NimbusMintegralKit", package: "nimbus-ios-sdk"),
-                .product(name: "NimbusMolocoKit", package: "nimbus-ios-sdk"),
-                .product(name: "NimbusInMobiKit", package: "nimbus-ios-sdk")
+                .product(name: "NimbusAPSKit", package: "nimbus-ios-aps"),
+                .product(name: "NimbusMetaKit", package: "nimbus-ios-meta"),
+                .product(name: "NimbusVungleKit", package: "nimbus-ios-vungle"),
+                .product(name: "NimbusUnityKit", package: "nimbus-ios-unity"),
+                .product(name: "NimbusMobileFuseKit", package: "nimbus-ios-mobilefuse"),
+                .product(name: "NimbusAdMobKit", package: "nimbus-ios-admob"),
+                .product(name: "NimbusMintegralKit", package: "nimbus-ios-mintegral"),
+                .product(name: "NimbusMolocoKit", package: "nimbus-ios-moloco"),
+                .product(name: "NimbusInMobiKit", package: "nimbus-ios-inmobi")
             ],
             path: "Sources")
     ]
